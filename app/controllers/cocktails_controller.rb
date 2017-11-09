3f8047b23f691dfd3a48cd71efbeacafe2468d12
romain hallary <niamor@mm.st>
2017-11-09 13:43:20 +0000

    controllers working and rake green

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,11 +1,20 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
   def index
+    @cocktails = Cocktail.all
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -15,8 +24,32 @@ class CocktailsController < ApplicationController
   end
 
   def update
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktails_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :description)
   end
 end
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'doses/new'
 
-  get 'doses/create'
+  root to: 'cocktails#index'
 
-  get 'doses/edit'
-
-  get 'doses/update'
-
-  get 'doses/destroy'
-
-  resources :ingredients
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [ :new, :create, :index, :show] do
+    resources :doses, only: [ :new, :create ]
+  end
+  resources :doses, only: [ :destroy ]
 end
