e788af326ad1a0a7c32f9aaec55f9011fee4a5a0
romain hallary <niamor@mm.st>
2017-11-10 11:09:09 +0000

    Prepare for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171109170430) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -22,8 +25,8 @@ ActiveRecord::Schema.define(version: 20171109170430) do
 
   create_table "doses", force: :cascade do |t|
     t.text "description"
-    t.integer "ingredient_id"
-    t.integer "cocktail_id"
+    t.bigint "ingredient_id"
+    t.bigint "cocktail_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
@@ -36,4 +39,6 @@ ActiveRecord::Schema.define(version: 20171109170430) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
